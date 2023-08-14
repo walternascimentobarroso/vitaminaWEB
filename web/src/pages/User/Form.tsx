4c0b46477235d486e51af33ecc07f74255af9a5e
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-08-14 13:48:26 +0100

    ✨ NEW FEATURE: List users

--- web/src/pages/User/Form.tsx
+++ web/src/pages/User/Form.tsx
@@ -2,33 +2,14 @@ import { useState } from "react";
 import Alert from "../../components/Alert";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
-import ProfilePicture from "../../components/ProfilePicture";
-import CustomSelect from "../../components/CustomSelect";
-import PasswordInput from "../../components/PasswordInput";
 
 export default ({ onActionSubmit, defaultValue = {}, closeModal }: any) => {
   const [errors, setErrors] = useState("");
   const [formState, setFormState] = useState(
     defaultValue || {
-      name: "",
-      lastName: "",
-      email: "",
-      password: "",
-      role: "",
-      status: "",
+      description: "",
     }
   );
-
-  const options = [
-    { value: "1", label: "Admin" },
-    { value: "2", label: "Manager" },
-    { value: "3", label: "User" },
-  ];
-
-  const handleSelect = (value: string) => {
-    console.log("Opção selecionada:", value);
-    // Faça algo com o valor selecionado, como atualizar o estado do componente pai
-  };
 
   const validateForm = () => {
     setErrors("");
@@ -65,48 +46,12 @@ export default ({ onActionSubmit, defaultValue = {}, closeModal }: any) => {
       )}
 
       <form className="p-1 bg-white dark:bg-gray-800">
-        <div className="flex justify-center">
-          <ProfilePicture alt="Profile picture" />
-        </div>
-
         <Input
-          label={"Name"}
-          placeholder={"Name"}
-          name={"name"}
-          value={formState?.name || ""}
+          label={"Description"}
+          placeholder={"Description"}
+          name={"description"}
+          value={formState?.description || ""}
           onChange={handleChange}
-        />
-
-        <Input
-          label={"Last Name"}
-          placeholder={"Last Name"}
-          name={"lastName"}
-          value={formState?.lastName || ""}
-          onChange={handleChange}
-        />
-
-        <Input
-          label={"Email"}
-          type={"email"}
-          placeholder={"Email"}
-          name={"email"}
-          value={formState?.email || ""}
-          onChange={handleChange}
-        />
-
-        <PasswordInput
-          label={"Password"}
-          placeholder={"Password"}
-          name={"password"}
-          value={formState?.password || ""}
-          onChange={handleChange}
-        />
-
-        <CustomSelect
-          label={"Role"}
-          name={"role"}
-          options={options}
-          onSelect={handleSelect}
         />
 
         <Button customClass="w-full" onClick={handleSubmit}>
--- web/src/pages/User/Page.tsx
+++ web/src/pages/User/Page.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./Form";
 import Table from "./Table";
 import Card from "../../components/Card";
@@ -12,7 +12,10 @@ import SearchButton from "../../components/SearchButton";
 import { useModal } from "../../hooks/useModal";
 import Spinner from "../../components/Spinner";
 
+import axios from 'axios';
+
 export default () => {
+  const router = "http://127.0.0.1/api/users";
   const { openModal, closeModal, ModalWrapper } = useModal();
   const [list, setList]: any = useState([]);
   const [loading, setLoading]: any = useState(false);
@@ -25,32 +28,32 @@ export default () => {
     };
     setList([...list, toastProperties]);
   };
-  const [data, setData] = useState([
-    {
-      id: 1,
-      name: "Administrator",
-      photo:
-        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-      email: "admin@gmail.com",
-      role: "Admin",
-    },
-    {
-      id: 2,
-      name: "User",
-      photo:
-        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-      email: "user@gmail.com",
-      role: "User",
-    },
-    {
-      id: 3,
-      name: "Manager",
-      photo:
-        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-      email: "manager@gmail.com",
-      role: "Manager",
-    },
-  ]);
+  const [data, setData]:any = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const token = localStorage.getItem('user_token');
+        const response = await axios.get(router, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const formatData = response.data.data.map((data:any) => ({
+          id: data.id,
+          name: data.name,
+        }));
+  
+        setData(formatData);
+        setFilteredData(formatData);
+      } catch (error) {
+        console.error('Erro ao buscar usuários:', error);
+      }
+    };
+  
+    fetchUsers();
+  }, []);
 
   const [rowToEdit, setRowToEdit] = useState({});
 
@@ -59,16 +62,28 @@ export default () => {
     setRowToEdit(row);
   };
 
-  const handleDeleteRow = (targetIndex: any) => {
+  const handleDeleteRow = async (targetIndex: any) => {
     setLoading(true);
-    setTimeout(() => {
+  
+    try {
+      const token = localStorage.getItem('user_token');
+      await axios.delete(`${router}/${targetIndex}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
       setData(data.filter((row: any) => row.id !== targetIndex));
       setFilteredData(
         filteredData.filter((row: any) => row.id !== targetIndex)
       );
       showToast("success", "Removed", "Removed with success");
+    } catch (error) {
+      console.error('Error deleting row:', error);
+      showToast("error", "Error", "Error while deleting");
+    } finally {
       setLoading(false);
-    }, 1000);
+    }
   };
 
   const handleSubmit = (newRow: any) => {
@@ -82,10 +97,11 @@ export default () => {
   };
 
   const editRegister = (newRow: any) => {
-    setData(data.map((row) => (row.id !== newRow.id ? row : newRow)));
+    setData(data.map((row:any) => (row.id !== newRow.id ? row : newRow)));
     setFilteredData(
-      filteredData.map((row) => (row.id !== newRow.id ? row : newRow))
+      filteredData.map((row:any) => (row.id !== newRow.id ? row : newRow))
     );
+
     showToast("success", "Edited", "Edit with success");
   };
 
@@ -97,7 +113,7 @@ export default () => {
 
   const [filteredData, setFilteredData] = useState(data);
   const filterData = ({ target }: any) => {
-    const filteredData = data.filter((item) =>
+    const filteredData = data.filter((item:any) =>
       item.name.toLowerCase().includes(target.value.toLowerCase())
     );
     setFilteredData(filteredData);
@@ -112,19 +128,22 @@ export default () => {
             href: "/home",
           },
           {
-            label: "User",
+            label: "Sellers",
             href: "",
           },
         ]}
       />
 
-      <Title>Users</Title>
+      <Title>Sellers</Title>
       {loading && <Spinner />}
       <Toast toasties={list} position="top-right" setList={setList} />
 
       <Card className="p-0">
         <div className="flex justify-between bg-white p-4 border-b dark:bg-gray-800 dark:border-gray-700 rounded-t-lg">
-          <SearchButton placeholder="Search for name" onChange={filterData} />
+          <SearchButton
+            placeholder="Search for description"
+            onChange={filterData}
+          />
 
           <Button
             onClick={() => {
@@ -143,7 +162,7 @@ export default () => {
         />
       </Card>
 
-      <ModalWrapper title="User">
+      <ModalWrapper title="Product">
         <Form
           onActionSubmit={handleSubmit}
           defaultValue={rowToEdit}
